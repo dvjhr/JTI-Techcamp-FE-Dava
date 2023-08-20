bef7eba42d424769fef7aefcbbf1238c4197d480
Dava Aditya Jauhar <57520495+dvjhr@users.noreply.github.com>
2023-08-20 21:20:56 +0700

    Update environment.ts
    Edit base url to BE API

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseURL: 'http://localhost:3000/api/v1/bootcamp'
+    baseURL: 'https://jti-techcamp-be-dava-git-mntbdjw-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/api/v1/bootcamp'
   };
   
